c6db9dc3b38536e21ebac165a4120f468ed947f6
JoseUri <joseuriarte323@gmail.com>
2017-01-31 22:18:40 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
 
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
+const words = document.getElementsByTagName('span')
+const cite = document.getElementsByTagName('cite')
+
+const animate = () => {
+  let maxDelay = 0
+  let maxDuration = 0
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i]
+    const duration = (word.dataset.duration)
+    const delay = (word.dataset.delay)
+    const blur = word.dataset.blur
+
+    maxDelay = Math.max(delay, maxDelay)
+    maxDuration = Math.max(duration, maxDuration)
+    
+    TweenLite.set(word, {
+      'webkitFilter': `blur(${blur}px)`
+    })
+    TweenLite.set(word, {
+      className:"+=animate",
+      transition: `all ${duration}s ease-in ${delay}s`
+    })
+  }
+  
+  TweenLite.set(cite, {
+    className:"+=animate",
+    transition: `all ${maxDuration}s ease-in ${maxDelay}s`
+  })
+  
+  TweenLite.delayedCall((maxDuration + maxDelay), () => {
+    const baseDelay = 3
+    TweenLite.set(words, { className:"-=animate", delay: baseDelay });
+    TweenLite.set(cite, { className:"-=animate", delay: (baseDelay) });
+    TweenLite.delayedCall((baseDelay + (maxDuration*2)), animate)
+  })
 }
+
+animate()
